a6f99dfa3bdaf231091612418a34bdb590018912
Saikat <saikat25112000@gmail.com>
2024-07-15 18:54:25 +0600

    Project setup done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDl9F6G_-2JQtnz0QD_ZzZaHphX3VFlYaA",
+  authDomain: "quiz-app-3b993.firebaseapp.com",
+  projectId: "quiz-app-3b993",
+  storageBucket: "quiz-app-3b993.appspot.com",
+  messagingSenderId: "527261833828",
+  appId: "1:527261833828:web:1908b9ac0f400b0ba85d71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
